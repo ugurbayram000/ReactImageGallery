843b2f21c0a0faccd5d3cfb072bfcc732ccbd863
ugostaki <ugostaki07@gmail.com>
2023-11-20 23:35:19 +0300

    ReactProject
    Image Gallery

---
+++ src/Images.js
@@ -0,0 +1,7 @@
+export default [
+  "https://www.kulturportali.gov.tr/contents/images/cavzer.jpg?format=jpg&quality=50&width=1200",
+  "https://www.tourismjournal.com.tr/static/re/resim-2023-08-22-082712814-1692682035-648-1692682955-990.jpeg",
+  "https://cdn.yemek.com/uploads/2015/11/sirin.jpg",
+  "https://blog.biletbayi.com/wp-content/uploads/2017/08/elazig-iclikofte.jpg",
+  "https://www.gidelimmi.com/wp-content/uploads/2023/02/lobik-corbasi.jpg",
+];
